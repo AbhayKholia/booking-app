0eeeb08cb7c6292549490ca25fe05f6cfe48c76f
Abhay Kholia <kholiaabhay@gmail.com>
2025-08-27 22:19:01 +0530

    api-intregate-changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,15 @@
+// import axios from "axios";
+
+// const API_BASE = "http://localhost:5000/api";
+
+// export const api = axios.create({
+//   baseURL: API_BASE,
+// });
+
+
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = "https://api-booking-ve-be.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
